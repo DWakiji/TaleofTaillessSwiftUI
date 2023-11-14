bb1961c856bd4f82b0759a8de6f67f6bb3a4183d
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2023-11-14 13:40:03 -0500

    new branch

---
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -0,0 +1,18 @@
+//
+//  Chapter2View.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Angelo Brown on 11/14/23.
+//
+
+import SwiftUI
+
+struct Chapter2View: View {
+    var body: some View {
+        Text("Chapter 2")
+    }
+}
+
+#Preview {
+    Chapter1View()
+}
