a7058e836d307ecaebe204d67ad74d311c2c9798
Angelo Brown <130711987+angelob120@users.noreply.github.com>
2023-11-15 14:47:22 -0500

    add text around box

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -8,29 +8,69 @@
 import SwiftUI
 
 struct Chapter2View: View {
+    // Define options
+    enum Option: String, CaseIterable {
+        case option1 = "Option 1"
+        case option2 = "Option 2"
+    }
+    
+    @State private var selectedOption = Option.option1
+    
     var body: some View {
-    Color()
         ScrollView {
             VStack {
-                Image("your_top_image") // Replace "your_top_image" with the name of your top image asset
+                // Top image
+                Image("your_top_image")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(height: 200) // Adjust the height as needed
+                    .frame(height: 200)
                 
-                Text("Chapter 2")
-                    .font(.largeTitle)
-                    .padding()
-
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui faucibus in ornare quam viverra orci sagittis eu. Rhoncus mattis rhoncus urna neque viverra. Tellus in metus vulputate eu scelerisque felis. Massa tincidunt nunc pulvinar sapien et. Ac turpis egestas sed tempus urna et pharetra pharetra. In nulla posuere sollicitudin aliquam ultrices. Habitasse platea dictumst vestibulum rhoncus. Nibh venenatis cras sed felis eget velit aliquet sagittis id. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Pellentesque diam volutpat commodo sed egestas egestas. Ornare suspendisse sed nisi lacus sed viverra tellus. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Urna nunc id cursus metus aliquam. Dolor sit amet consectetur adipiscing elit pellentesque. Et ultrices neque ornare aenean. Sed vulputate odio ut enim blandit volutpat maecenas.")
-                     
-
-                    .font(.body)
-                    .padding()
-
-                Image("your_bottom_image") // Replace "your_bottom_image" with the name of your bottom image asset
+                // Text content inside a box with a ScrollView
+                VStack {
+                    Text("Chapter 2")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    ZStack {
+                        Color.white // Background color of the box
+                            .cornerRadius(10) // Rounded corners for the box
+                            .shadow(radius: 5) // Add shadow for depth
+                        
+                        ScrollView {
+                            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui faucibus in ornare quam viverra orci sagittis eu. Rhoncus mattis rhoncus urna neque viverra. Tellus in metus vulputate eu scelerisque felis. Massa tincidunt nunc pulvinar sapien et. Ac turpis egestas sed tempus urna et pharetra pharetra. In nulla posuere sollicitudin aliquam ultrices. Habitasse platea dictumst vestibulum rhoncus. Nibh venenatis cras sed felis eget velit aliquet sagittis id. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Pellentesque diam volutpat commodo sed egestas egestas. Ornare suspendisse sed nisi lacus sed viverra tellus. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Urna nunc id cursus metus aliquam. Dolor sit amet consectetur adipiscing elit pellentesque. Et ultrices neque ornare aenean. Sed vulputate odio ut enim blandit volutpat maecenas.")
+                                .font(.body)
+                                .padding()
+                        }
+                    }
+                    .padding(10) // Padding within the box
+                }
+                
+                // Option selector buttons
+                Picker("Choose an option", selection: $selectedOption) {
+                    ForEach(Option.allCases, id: \.self) { option in
+                        Text(option.rawValue)
+                            .tag(option)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                // Display content based on the selected option
+                if selectedOption == .option1 {
+                    Text("Content for Option 1 goes here...")
+                        .font(.body)
+                        .padding()
+                } else if selectedOption == .option2 {
+                    Text("Content for Option 2 goes here...")
+                        .font(.body)
+                        .padding()
+                }
+                
+                // Bottom image
+                Image("your_bottom_image")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(height: 200) // Adjust the height as needed
+                    .frame(height: 200)
             }
         }
         .navigationBarTitle("Chapter 2", displayMode: .inline)
@@ -42,3 +82,4 @@ struct Chapter2View_Previews: PreviewProvider {
         Chapter2View()
     }
 }
+
