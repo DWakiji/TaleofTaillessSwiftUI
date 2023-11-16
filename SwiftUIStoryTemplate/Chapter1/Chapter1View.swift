d33b9497055d1c8258771e553f751ebc170378ce
Nychelle <90415090@thedps.org>
2023-11-16 09:56:25 -0500

    Ignore this

--- SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
+++ SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
@@ -1,11 +1,21 @@
 import SwiftUI
 
+//Add Chapter picture(square)
+//Add chapter one as a tittle *
+//Navagation bar
+//Tab bar with pages and when you click on it the pages show
+//Back Button
+//Done button
+//Next Button
+
 struct Chapter1View: View {
     var body: some View {
-        Text("Chapter 1")
+        Text("Chapter One")
     }
 }
 
 #Preview {
     Chapter1View()
-}
+    }
+
+
